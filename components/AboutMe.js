eff2bbe6f4d516ac14a91dbf7d6781c53441b5e8
Jordan Barrand <Jordan.barrand@outlook.com>
2021-06-18 17:31:08 +0100

    FIxed bug

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -18,13 +18,9 @@ export default function AboutMe() {
 			<TextContainer variants={scrollReveal} ref={ref} animate={controls} initial='hidden'>
 				<H2>&#60; Get to know me /&#62;</H2>
 				<P>
-
 					Hey! I'm Jordan, a 20 year old from the U.K, who is currently aspiring to be a
 					Web Developer / Software Engineer. My goal is to solve problems for people who
 					can't and to create products which make life easier.
-
-
-
 				</P>
 			</TextContainer>
 		</Container>
